7a5a411561e4a9443a7a768df11762928a204cf1
John Powell <john@Johns-MacBook-Pro.local>
2023-11-09 09:48:49 -0600

    added error messaging and changed the URL paths

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -8,16 +8,39 @@ function App() {
 
   const [player, setPlayer] = useState(0)
   const [newPosition, setNewPosition] = useState("")
+  const [formError, setFormError] = useState("")
+  const [moving, setMoving] = useState(false)
 
   useEffect(() => {
 
     switch (window.location.pathname) {
-      case "/slong/player1": setPlayer(1); break;
-      case "/slong/player2": setPlayer(2); break;
+      case "/player1": setPlayer(1); break;
+      case "/player2": setPlayer(2); break;
       default: break;
     }
 
   }, [window.location.pathname])
+
+  const getBase = () => window.location.host == "jepmac.local" ? window.location.host : "jepmac"
+
+  const movePaddle = () => {
+
+    setMoving(true)
+
+    axios.post(`http://${getBase()}:8080/move/player` + player, { position: +newPosition })
+      .then((res) => {
+        setMoving(false)
+        console.log(res.data)
+      })
+      .catch((err) => {
+        setMoving(false)
+        console.log("error code:", err?.code)
+        console.log("error message:", err?.message)
+        console.log("error status:", err?.response?.status)
+        console.log("error result:", err?.response?.data?.result)
+        setFormError(err?.response?.data?.result || "an error occured while processing your request")
+      })
+  }
 
   return (
     <div className='flex justify-center'>
@@ -26,35 +49,50 @@ function App() {
           <span className='text-3xl font-bold'>Slong</span>
           <span className='text-base font-bold'>(Slow Pong)</span>
 
-          <div className='flex flex-col mt-4 p-4 border border-solid rounded-md border-gray-400'>
+          {player > 0 &&
+            <div className='flex flex-col mt-4 p-4 border border-solid rounded-md border-gray-400'>
 
-            <span className='font-bold my-4'>Player {player}</span>
+              <span className='font-bold my-4'>Player {player}</span>
 
-            <div className='flex flex-col w-full'>
-              <form className='flex flex-col items-center w-full' onSubmit={(e) => {
-                e.preventDefault()
-                axios.post("http://jepmac:8080/move/player" + player, { position: +newPosition }).then((res) => {
-                  console.log("JEP response:", res.data)
-                }).catch((err) => {
-                  console.error("JEP error:", err)
-                })
-              }}>
-                <input
-                  name='position-input'
-                  type="text"
-                  placeholder="enter position"
-                  className="input input-bordered w-full max-w-sm"
-                  maxLength={2}
-                  onChange={(e) => {
-                    setNewPosition(e.target.value)
-                  }} />
-                <div className='flex justify-end max-w-sm w-full mt-2'>
-                  <button className="btn btn-primary">Move Paddle</button>
-                </div>
-              </form>
+              <div className='flex flex-col w-full'>
+                <form className='flex flex-col items-center w-full' onSubmit={(e) => {
+                  e.preventDefault()
+                  if (moving) {
+                    return
+                  }
+                  movePaddle()
+                }}>
+                  <div className='flex flex-col w-full items-center'>
+                    <input
+                      name='position-input'
+                      type="text"
+                      placeholder="enter position"
+                      className={`input border border-solid
+                        ${formError ? "border-red-500" : "border-gray-400"}
+                        w-full max-w-sm`}
+                      maxLength={2}
+                      onChange={(e) => {
+                        setFormError("")
+                        setNewPosition(e.target.value)
+                      }} />
+                    <span className='text-sm text-red-500 font-bold w-full max-w-sm text-start'>
+                      {formError}
+                    </span>
+                  </div>
+                  <div className='flex justify-end max-w-sm w-full mt-4'>
+                    <button className="btn btn-primary w-full md:w-36">
+                      {
+                        moving
+                          ? <span class="loading loading-spinner loading-sm"></span>
+                          : "Move Paddle"
+                      }
+                    </button>
+                  </div>
+                </form>
+              </div>
+
             </div>
-
-          </div>
+          }
         </div>
       </div>
     </div>
